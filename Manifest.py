49b23876e990d15d734888032164c4ed68af0f55
Grzegorz Daniluk <g.daniluk@elproma.com.pl>
2012-02-08 16:49:16 +0100

    adding wrsw_txtsu module

--- Manifest.py
+++ Manifest.py
@@ -1,7 +1,8 @@
 fetchto = "ip_cores"
 
 modules =  {"local" : 
-    [ "modules/wrsw_nic" ],
+    [ "modules/wrsw_nic",
+      "modules/wrsw_txtsu" ],
     "git" : "git://ohwr.org/hdl-core-lib/wr-cores.git::wrcore_v2",
     "svn" : "http://svn.ohwr.org/gn4124-core/branches/hdlmake-compliant/rtl"
     }
