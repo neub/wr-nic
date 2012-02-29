000e2b99bec6593f3cf2d5a44b92c35cc4bdac71
Grzegorz Daniluk <g.daniluk@elproma.com.pl>
2012-02-29 09:51:20 +0100

    updated Manifest to use wishbonized wrcore

--- Manifest.py
+++ Manifest.py
@@ -3,6 +3,6 @@ fetchto = "ip_cores"
 modules =  {"local" : 
     [ "modules/wrsw_nic",
       "modules/wrsw_txtsu" ],
-    "git" : "git://ohwr.org/hdl-core-lib/wr-cores.git::wrcore_v2",
+    "git" : "git://ohwr.org/hdl-core-lib/wr-cores.git::wishbonized",
     "svn" : "http://svn.ohwr.org/gn4124-core/branches/hdlmake-compliant/rtl"
     }
