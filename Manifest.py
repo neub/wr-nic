7b871e857dd0e3ea92830048b232c2e46bd656fa
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-03-14 01:29:09 +0100

    Changed gn4124_core branch to master

--- Manifest.py
+++ Manifest.py
@@ -2,7 +2,9 @@ fetchto = "ip_cores"
 
 modules =  {"local" : 
     [ "modules/wrsw_nic",
-      "modules/wrsw_txtsu" ],
+      "modules/wrsw_txtsu",
+      "modules/wrsw_dio"
+       ],
     "git" : "git://ohwr.org/hdl-core-lib/wr-cores.git::wishbonized",
-    "svn" : "http://svn.ohwr.org/gn4124-core/branches/hdlmake-compliant/rtl"
+    "svn" : "http://svn.ohwr.org/gn4124-core/trunk/hdl/gn4124core/rtl"
     }
