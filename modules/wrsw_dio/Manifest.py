8cdf35a4d626e3033fb5fd15a16a1b5dcd9ea038
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2012-03-14 01:30:11 +0100

    modules/wrsw_dio: removed unnecessary files from Manifest

--- modules/wrsw_dio/Manifest.py
+++ modules/wrsw_dio/Manifest.py
@@ -1,5 +1,3 @@
 files = ["wrsw_dio_wb.vhd",
 				  "wrsw_dio.vhd",
-				  "dummy_time.vhd", 
-                                  "wrsw_dio.wb",
-                                  "wrsw_dio_wb.htm"]
+				  "dummy_time.vhd" ]
