a21ed786eed896b7380253c6e5c8063bc0b8e4b8
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2013-07-05 17:21:09 +0200

    little cleanup in Manifests

--- Manifest.py
+++ Manifest.py
@@ -1,6 +1,7 @@
 fetchto = "ip_cores"
 
 modules =  { "local" : ["modules/wrsw_dio",
+		      "platform/xilinx",
 		      "ip_cores/wr-switch-hdl",
 		      "ip_cores/general-cores",
 		      "ip_cores/gn4124-core/hdl/gn4124core/rtl"]
--- top/spec/Manifest.py
+++ top/spec/Manifest.py
@@ -1,7 +1,3 @@
 files = ["wr_nic_sdb_top.vhd", "wr_nic_sdb_top.ucf", "spec_serial_dac.vhd", "spec_serial_dac_arb.vhd" ]
 
-fetchto = "../../ip_cores"
-
-modules = {
-	   "local" : ["../../"]
-	}
+modules = { "local" : [ "../../" ] }
