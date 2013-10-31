9e056fc6158fb334ccfd49baa605675e66159f5e
Miguel Jimenez Lopez <mjl.klyone@gmail.com>
2013-10-31 12:32:05 +0100

    etherbone-core: added as submodule

--- Manifest.py
+++ Manifest.py
@@ -4,7 +4,8 @@ modules =  { "local" : ["modules/wrsw_dio",
 		      "platform/xilinx",
 		      "ip_cores/wr-switch-hdl",
 		      "ip_cores/general-cores",
-		      "ip_cores/gn4124-core/hdl/gn4124core/rtl"]
+		      "ip_cores/gn4124-core/hdl/gn4124core/rtl",
+		      "ip_cores/etherbone-core"]
            }
 
 
